2c9d0b9bf9952abc75fb0a8e96ae5a74d6bf72ec
raldblox <raldblox@outlook.com>
2023-03-17 10:04:52 +0800

    config hardhat for deployment

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,6 +1,37 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 module.exports = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.10",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    mumbai: {
+      url: process.env.ALCHEMY_MUMBAI,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      url: process.env.ALCHEMY_MAINNET,
+      accounts: [process.env.PRIVATE_KEY_MAIN],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN,
+  },
 };
